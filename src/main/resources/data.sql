772ca9a1e7fb4f8dcc21220e35f863177d23f6c8
IIVRIICOLKM <djrlee1@naver.com>
2024-09-20 17:23:16 +0900

    Q7

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q2', 'C2',
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q3', 'C3', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q4', 'C4', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q5', 'C5', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q6', 'C6', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q7', 'C7', NOW(), NOW());
