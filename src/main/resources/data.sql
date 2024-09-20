13923251c670da8b649a01a390810b30187d35ac
IIVRIICOLKM <djrlee1@naver.com>
2024-09-20 16:38:26 +0900

    돼라 좀 ㅅㅂ

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,3 @@ INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q1', 'C1',
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q2', 'C2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q3', 'C3', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q4', 'C4', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q5', 'C5', NOW(), NOW());
