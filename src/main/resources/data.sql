167971cb99a867207a587b36d8625b6f3338ce4e
IIVRIICOLKM <djrlee1@naver.com>
2024-09-20 16:10:59 +0900

    서브 브렌치에 db 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q1', 'C1', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q2', 'C2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q3', 'C3', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q4', 'C4', NOW(), NOW());
