679f64925b64a4bf48320e042df6b6fb840c6cb6
IIVRIICOLKM <djrlee1@naver.com>
2024-09-22 23:13:40 +0900

    토큰 생성, 유효성 검증 로직 생성

--- src/main/java/Blog/service/UserService.java
+++ src/main/java/Blog/service/UserService.java
@@ -18,4 +18,8 @@ public class UserService {
                 //패스워드 암호화
                 .password(bCryptPasswordEncoder.encode(dto.getPassword())).build()).getId();
     }
+
+    public User findById(Long userId){
+        return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected User"));
+    }
 }
