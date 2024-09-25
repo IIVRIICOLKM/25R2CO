0b5be702d378cf820118b22a11978454a46cbe66
IIVRIICOLKM <djrlee1@naver.com>
2024-09-25 20:19:37 +0900

    Q8 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q3', 'C3',
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q4', 'C4', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q5', 'C5', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q7', 'C7', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Q8', 'C8', NOW(), NOW());
